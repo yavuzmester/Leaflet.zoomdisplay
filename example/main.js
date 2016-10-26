b18e320a8f44567f0c7b10f8b084e3f5a8c2be4d
yavuzmester <yavuzmester@gmail.com>
2016-10-26 16:13:51 +0300

    in progress

--- example/main.js
+++ example/main.js
@@ -1,7 +1,10 @@
 "use strict";
 
-const ReactDOM = require("react-dom");
 const React = require("react");
+const ReactDOM = require("react-dom");
 const {GeoMap} = require("components");
 
-ReactDOM.render(React.createElement(GeoMap, {center: [51.505, -0.09], zoom: 13}), document.getElementById("container"));
+ReactDOM.render(
+    React.createElement(GeoMap, {center: [51.505, -0.09], zoom: 13}),
+    document.getElementById("container")
+);
